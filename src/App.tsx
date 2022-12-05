cb70e794e302a2c7c1fc3b3e9a55f003e157f6eb
Elizabeth Lands <elizabeth_lands@hotmail.com>
2022-12-05 15:26:34 +0000

    Feat: basic layout, styles & header

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import Header from "./Header.svg";
+
+const HeaderWrapper = styled.div`
+  padding: 80px;
+  display: flex;
+  justify-content: center;
+`;
+
+const InnerContainer = styled.div`
+  width: 80%;
+  max-width: 900px;
+  margin: 0 auto;
+`;
+
+const OptionsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 50px;
+`;
+
+const StyledSelect = styled.select``;
+
+const StyledButton = styled.button`
+  padding: 15px;
+  border: none;
+  border-radius: 12px;
+  font-size: 1rem;
+  &:hover,
+  &:active,
+  &:focus {
+    color: blue;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <HeaderWrapper>
+        <img src={Header} alt="I love dogs" />
+      </HeaderWrapper>
+      <InnerContainer>
+        <OptionsContainer>
+          <StyledSelect>Show me my favourite breed</StyledSelect>
+          <StyledButton>Show me all the dogs</StyledButton>
+          <StyledButton>Show me just one dog</StyledButton>
+        </OptionsContainer>
+      </InnerContainer>
+    </>
   );
 }
-
 export default App;
---
+++ src/components/Button.tsx
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+const Button = () => {};
+
+export default Button;
